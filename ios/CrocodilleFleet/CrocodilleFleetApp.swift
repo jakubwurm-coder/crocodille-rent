5d0eac010973b838ddf8dad0327c701ee17c7534
jakubwurm-coder <jakubwurm@gmail.com>
2026-09-03 00:32:44 +0200

    Show notifications while app is open

--- ios/CrocodilleFleet/CrocodilleFleetApp.swift
+++ ios/CrocodilleFleet/CrocodilleFleetApp.swift
@@ -1,8 +1,28 @@
 import SwiftUI
+import UIKit
 import UserNotifications
+
+final class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .list, .sound, .badge])
+    }
+}
 
 @main
 struct CrocodilleFleetApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var store = FleetStore()
 
     var body: some Scene {
